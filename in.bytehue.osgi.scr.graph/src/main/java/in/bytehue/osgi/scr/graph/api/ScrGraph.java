10a79ff378a914b640f8efe4f58d63f306197edc
Amit Kumar Mondal <admin@amitinside.com>
2021-02-02 08:06:42 +0100

    Updated API

--- in.bytehue.osgi.scr.graph/src/main/java/in/bytehue/osgi/scr/graph/api/ScrGraph.java
+++ in.bytehue.osgi.scr.graph/src/main/java/in/bytehue/osgi/scr/graph/api/ScrGraph.java
@@ -49,11 +49,11 @@ public interface ScrGraph {
      * Returns a graph from the Service Component Runtime (SCR).
      *
      * <p>
-     * By default, it will not include any service reference which
+     * <b>Note that</b>,, it will not include any service reference which
      * is not exported using OSGi Declarative Services.
      *
      * <p>
-     * <b>Note that</b>, this is an <b>idempotent operation</b> and
+     * <b>Also note that</b>, this is an <b>idempotent operation</b> and
      * hence multiple invocations of this method will result in same results.
      *
      * @return the {@link Graph} instance
@@ -63,28 +63,13 @@ public interface ScrGraph {
     Graph<ScrComponent, DefaultEdge> getGraph();
 
     /**
-     * Returns a graph from the Service Component Runtime (SCR).
-     *
-     * <p>
-     * <b>Note that</b>, this is an <b>idempotent operation</b> and
-     * hence multiple invocations of this method will result in same results.
-     *
-     * @param includeNonScrServiceReferences flag to include any service reference
-     *            which is not exported using OSGi Declarative Services
-     *
-     * @return the {@link Graph} instance
-     *
-     * @see #getGraph()
-     */
-    Graph<ScrComponent, DefaultEdge> getGraph(boolean includeNonScrServiceReferences);
-
-    /**
      * Returns a list containing all the list of components that
      * for a cycle (SCR).
      *
      * <p>
      * <b>Note that</b>, by default it makes use of Tarjan algorithm to
-     * find out all the cycles
+     * find out all the cycles and it will not include any service reference
+     * which is not exported using OSGi Declarative Services
      *
      * <p>
      * <b>Also note that</b>, this is an <b>idempotent operation</b> and
@@ -95,28 +80,6 @@ public interface ScrGraph {
      * @see #getCycles(boolean)
      */
     List<List<ScrComponent>> getCycles();
-
-    /**
-     * Returns a list containing all the list of components that
-     * for a cycle (SCR).
-     *
-     * <p>
-     * <b>Note that</b>, by default it makes use of Tarjan algorithm to
-     * find out all the cycles and it will not include any service reference
-     * which is not exported using OSGi Declarative Services.
-     *
-     * <p>
-     * <b>Also note that</b>, this is an <b>idempotent operation</b> and
-     * hence multiple invocations of this method will result in same results.
-     *
-     * @param includeNonScrServiceReferences flag to include any service reference
-     *            which is not exported using OSGi Declarative Services
-     *
-     * @return the {@link List} of all cycles (will never be {@code null})
-     *
-     * @see #getCycles()
-     */
-    List<List<ScrComponent>> getCycles(boolean includeNonScrServiceReferences);
 
     /**
      * Returns the graph containing all the cycles.
@@ -135,26 +98,6 @@ public interface ScrGraph {
      * @see #getCyclesAsGraph(boolean)
      */
     Graph<ScrComponent, DefaultEdge> getCyclesAsGraph();
-
-    /**
-     * Returns the graph containing all the cycles.
-     *
-     * <p>
-     * <b>Note that</b>, by default it makes use of Tarjan algorithm to
-     * find out all the cycles.
-     *
-     * <p>
-     * <b>Also note that</b>, this is an <b>idempotent operation</b> and
-     * hence multiple invocations of this method will result in same results.
-     *
-     * @param includeNonScrServiceReferences flag to include any service reference
-     *            which is not exported using OSGi Declarative Services
-     *
-     * @return the {@link Graph} instance
-     *
-     * @see #getCyclesAsGraph()
-     */
-    Graph<ScrComponent, DefaultEdge> getCyclesAsGraph(boolean includeNonScrServiceReferences);
 
     /**
      * Exports the specified graph into DOT representation.
